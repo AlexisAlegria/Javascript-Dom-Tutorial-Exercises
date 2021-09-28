e3645818d79c2a42ab6c44a357f911852ff27a65
AlexisAlegria <alexisalegriav@gmail.com>
2021-09-28 23:40:16 +0000

    up to exercise 13

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,5 +2,21 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+// your code here
+
+	let dropDown = document.querySelector('#mySelect');
+	for(var i=0; i<countries.length;i++){
+		var option = countries[i];
+		var el = document.createElement('option');
+		el.textContent = option;
+		el.value = option;
+		dropDown.appendChild(el);
+	}  
 };
+var selection = document.querySelector('#mySelect');
+selection.addEventListener('change', selectItem);
+function selectItem(e){
+	e.preventDefault();
+	let countrySelected = e.target.value;
+	alert(countrySelected);
+}
